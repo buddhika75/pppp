6bd239febb8ce4bb73afa263f402a8cc211b4c2d
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2020-11-29 09:39:28 +0530

    Improving Solution Profile Display

--- src/main/java/cwcdh/pppp/pojcs/Display.java
+++ src/main/java/cwcdh/pppp/pojcs/Display.java
@@ -34,11 +34,11 @@ import java.util.List;
 public class Display {
 
     private List<DisplayPlaceholder> placeholders;
-    
-    private Placeholder stringToEnum(String str){
-        try{
+
+    private Placeholder stringToEnum(String str) {
+        try {
             return Placeholder.valueOf(str);
-        }catch(Exception e){
+        } catch (Exception e) {
             return null;
         }
     }
@@ -47,7 +47,7 @@ public class Display {
         System.out.println("strph = " + strph);
         Placeholder ph = stringToEnum(strph);
         System.out.println("ph = " + ph);
-        if(ph==null){
+        if (ph == null) {
             return "";
         }
         String str = "";
@@ -64,7 +64,97 @@ public class Display {
         }
         return str;
     }
-    
+
+    public String getDisplayItemsAsString(String strph) {
+        List<DisplayItem> dis = getDisplayItemsStr(strph);
+        String s = "";
+
+        for (DisplayItem d : dis) {
+
+            if (d.getHtmlComponent() == null) {
+                s += "<span style=\"" + d.getStyle() + "\">" + d.getText() + "</span>" + "\n";
+                continue;
+            }
+
+            switch (d.getHtmlComponent()) {
+                case h1:
+                    s += "<h1 style=\"" + d.getStyle() + "\">" + d.getText() + "</h1>" + "\n";
+                    break;
+                case h2:
+                    s += "<h2 style=\"" + d.getStyle() + "\">" + d.getText() + "</h2>" + "\n";
+                    break;
+                case h3:
+                    s += "<h3 style=\"" + d.getStyle() + "\">" + d.getText() + "</h3>" + "\n";
+                    break;
+                case h4:
+                    s += "<h4 style=\"" + d.getStyle() + "\">" + d.getText() + "</h4>" + "\n";
+                    break;
+                case h5:
+                    s += "<h5 style=\"" + d.getStyle() + "\">" + d.getText() + "</h5>" + "\n";
+                    break;
+                case h6:
+                    s += "<h6 style=\"" + d.getStyle() + "\">" + d.getText() + "</h6>" + "\n";
+                    break;
+                case p:
+                    s += "<p style=\"" + d.getStyle() + "\">" + d.getText() + "</p>" + "\n";
+                    break;
+                case span:
+                    s += "<span style=\"" + d.getStyle() + "\">" + d.getText() + "</span>" + "\n";
+                    break;
+                case hr:
+                    s += "<hr style=\"" + d.getStyle() + "\"></hr>" + "\n";
+                    break;
+                case label:
+                    s += "<span style=\"" + d.getStyle() + "\">" + d.getText() + "</span>" + "\n";
+                    break;
+                case p_opening:
+                    s += "<p style=\"" + d.getStyle() + "\">";
+                    break;
+                case p_closing:
+                    s += "</p>" + "\n";
+                    break;
+                case span_opening:
+                    s += "<span style=\"" + d.getStyle() + "\">";
+                    break;
+                case span_closing:
+                    s += "</span>" + "\n";
+                    break;
+                case ul_opening:
+                    s += "<ul style=\"" + d.getStyle() + "\">";
+                    break;
+                case ul_closing:
+                    s += "</ul>" + "\n";
+                    break;
+                case ol_opening:
+                    s += "<ol style=\"" + d.getStyle() + "\">";
+                    break;
+                case ol_closing:
+                    s += "</ol>" + "\n";
+                    break;
+                case table_opening:
+                    s += "<table style=\"" + d.getStyle() + "\">";
+                    break;
+                case table_closing:
+                    s += "</table>" + "\n";
+                    break;
+                case ampersand:
+                    s += " & " + "\n";
+                    break;
+                case comma:
+                    s += ", " + "\n";
+                    break;
+                case line_break:
+                    s += "<br/>" + "\n";
+                default:
+                    s += d.getText() + "\n";
+
+            }
+
+        }
+        System.out.println("s = " + s);
+        return s;
+    }
+
     public List<DisplayItem> getDisplayItemsStr(String strph) {
         Placeholder ph = stringToEnum(strph);
         List<DisplayItem> tdis = new ArrayList<>();
